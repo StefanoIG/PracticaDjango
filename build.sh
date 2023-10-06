113dfd57580a85f114eb236402e39a11acae5b44
StefanoIG <99378605+StefanoIG@users.noreply.github.com>
2023-10-05 21:52:08 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-DJANGO_SUPERUSER_PASSWORD=somepassword python manage.py createsuperuser --no-input --username=joe  --email=joe@example.com
+DJANGO_SUPERUSER_PASSWORD=somepassword python manage.py createsuperuser --no-input --username=Stefano  --email=joe@example.com
